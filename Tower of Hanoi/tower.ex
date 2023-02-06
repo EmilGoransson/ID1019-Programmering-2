0fa8644e567508a2fcc3592220ed523ecae22c0a
EmilGoransson <emilgo@kth.se>
2023-02-06 15:05:23 +0100

    assignment done

--- Tower of Hanoi/tower.ex
+++ Tower of Hanoi/tower.ex
@@ -1,3 +1,11 @@
-defmodule tower do
+defmodule HanoiSolver do
+  def hanoi(0, _, _, _) do
+    []
+  end
 
+  def hanoi(n, from, aux, to) do
+    hanoi(n - 1, from, to, aux) ++
+      [{:move, from, to}] ++
+      hanoi(n - 1, aux, from, to)
+  end
 end
