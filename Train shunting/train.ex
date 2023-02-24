44c1ca49b2ad2c909db4dcdec0854316284f07a8
EmilGoransson <emilgo@kth.se>
2023-02-24 12:03:12 +0100

    added functionality for negative n to take & drop which fixed single/2 & sequence/2

--- Train shunting/train.ex
+++ Train shunting/train.ex
@@ -1,8 +1,20 @@
 defmodule Train do
   # returns the list containing the first n elements of the train.
+  def list_length([]) do
+    0
+  end
+
+  def list_length([_ | t]) do
+    list_length(t) + 1
+  end
 
   def take(_, 0) do
     []
+  end
+
+  ## case for negative n (bc of how i prev implemented single/2 i need this)
+  def take(trains, n) when n < 0 do
+    drop(trains, list_length(trains) + n)
   end
 
   def take([h | t], n) do
@@ -10,8 +22,14 @@ defmodule Train do
   end
 
   # returns the list xs without its first n elements
+
   def drop([], 0) do
     []
+  end
+
+  # case for negative n (needed bc of how implemented single/2 prev)
+  def drop(list, n) when n < 0 do
+    take(list, list_length(list) + n)
   end
 
   def drop([h | t], 0) do
@@ -61,17 +79,21 @@ defmodule Train do
   end
 
   # Explain this one in the report!
+
+  # basecase,if the input list is empty, return a tuple with the original value of and empty lists.
   def main([], n) do
     {n, [], []}
   end
 
+  # basecase 2, if n == 0 we return list.
   def main([h | t], 0) do
     {0, [h | t], []}
   end
 
+  # uses pattern matching.
   def main([h | t], n) do
     {k, remain, take} = main(t, n)
-
+    # check if k is 0 though pattern matching.
     if(k == 0) do
       {k, [h | remain], take}
     else
